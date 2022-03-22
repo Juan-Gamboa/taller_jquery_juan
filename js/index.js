9457062bd19b0524f777800bde77eaf1804ec575
unknown <juaestnino@uniboyaca.edu.co>
2022-03-22 16:04:49 -0500

    funcionamiento ejercicio contar letras

--- js/index.js
+++ js/index.js
@@ -1,10 +1,8 @@
 $(document).ready(()=>{
     $("#formCuenta").submit((ev)=> {
         ev.preventDefault();
-        var cantnum = $("#palabraInput").toArray()
-        for (i = 0; i < cantnum.length; i++) {
-            alert(cantnum[i].innerHTML);
-            }
-        alert("La palabra" +" "+$("#palabraInput").val()+" "+ "tiene"+" "+i+" "+"caracteres");
+        var cantnum =$.trim($("#palabraInput").val());
+        var resnum = cantnum.split("");
+        alert("La palabra" +" "+$("#palabraInput").val()+" "+ "tiene"+" "+resnum.length+" "+"caracteres");
     });
 })
